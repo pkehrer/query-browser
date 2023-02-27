0719ea0cd9a0bb09fb96cd8424cee1daa62370ad
Paul Kehrer <pkehrer@cimpress.com>
2023-02-27 10:34:56 -0500

    allow for separated query windows based on query parameters

--- src/app/browser/editor/editor.component.ts
+++ src/app/browser/editor/editor.component.ts
@@ -1,129 +1,88 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import * as _ from 'lodash'
-import { NgbTabChangeEvent, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
-const QUERY_KEY = 'tabbed-queries'
+import { ActivatedRoute } from '@angular/router';
 
 export interface EditorData {
-  title: string
-  text: string
+	text: string
 }
 
+const BASE_DEFAULT_QUERY = '`/* Write SQL Code here! */\n\nSELECT * FROM customer_details LIMIT 10\n\n`'
+
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'app-editor',
+	templateUrl: './editor.component.html',
+	styleUrls: ['./editor.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class EditorComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) {
-  }
+	constructor(private route: ActivatedRoute) { }
 
-  @Output() queryChanged = new EventEmitter<string>()
-  data: EditorData[]
-  activeTabId: string
-  editingTitle: string
+	@Output() queryChanged = new EventEmitter<string>()
+	data: EditorData[]
+	editingTitle: string
+	private queryKey: string
+	private defaultQuery: string
 
-  codemirrorOptions = {
-    lineNumbers: true,
-    theme: 'default',
-    mode: 'sql',
-    viewportMargin: Infinity
-  }
+	codemirrorOptions = {
+		lineNumbers: true,
+		theme: 'default',
+		mode: 'sql',
+		viewportMargin: Infinity
+	}
 
-  loadData(): void {
-    const dataString = localStorage.getItem(QUERY_KEY)
-    const parsed = this.parseQuery(dataString)
-    if (parsed) {
-      this.data = parsed
-    } else {
-      this.data = [{
-        title: 'untitled-1',
-        text: `/* Write SQL Code here! */\n\nSELECT * FROM customer_details LIMIT 10\n\n`
-      }]
-    }
-    this.activeTabId = this.data[0].title
-    this.saveData()
-    this.queryChanged.next(this.data[0].text)
-  }
+	loadData(): void {
+		const dataString = localStorage.getItem(this.queryKey)
+		const parsed = this.parseQuery(dataString)
+		if (parsed) {
+			this.data = parsed
+		} else {
+			this.data = [{
+				text: this.defaultQuery
+			}]
+		}
+		this.saveData()
+		this.queryChanged.next(this.data[0].text)
+	}
 
 
-  parseQuery(q: string): EditorData[] {
-    if (!q) {
-      return null
-    }
-    try {
-      const parsed = JSON.parse(q)
-      if (!(parsed instanceof Array)) {
-        throw new Error()
-      }
-      if (parsed.length === 0) {
-        throw new Error()
-      }
-      if (!_.every(parsed, (p: any) => 'title' in p && 'text' in p)) {
-        throw new Error()
-      }
-      return parsed
-    } catch {
-      return null
-    }
-  }
+	parseQuery(q: string): EditorData[] {
+		if (!q) {
+			return null
+		}
+		try {
+			const parsed = JSON.parse(q)
+			if (!(parsed instanceof Array)) {
+				throw new Error()
+			}
+			if (parsed.length === 0) {
+				throw new Error()
+			}
+			if (!_.every(parsed, (p: any) => 'title' in p && 'text' in p)) {
+				throw new Error()
+			}
+			return parsed
+		} catch {
+			return null
+		}
+	}
 
-  saveData(): void {
-    localStorage.setItem(QUERY_KEY, JSON.stringify(this.data))
-  }
+	saveData(): void {
+		localStorage.setItem(this.queryKey, JSON.stringify(this.data))
+	}
 
-  ngOnInit() {
-    this.loadData()
-  }
+	ngOnInit() {
+		this.route.queryParamMap.subscribe(map => {
+			this.queryKey = (map.get('query_key') || 'default') + '-query-key'
+			this.defaultQuery = map.get('default_query') || BASE_DEFAULT_QUERY
+			console.log(this.queryKey)
+			this.loadData()
+		})
+	}
 
-  addTab(event: MouseEvent) {
-    let tabNum = 0
-    let existingTab
-    do {
-      tabNum++
-      existingTab = _.find(this.data, (t: EditorData) => t.title === `untitled-${tabNum}`)
-    } while (!_.isNil(existingTab))
-    this.data.push({ title: `untitled-${tabNum}`, text: '' })
-    this.activeTabId = _.last(this.data).title
-    this.saveData()
-    this.queryChanged.next("")
-  }
-
-  textChange(data: EditorData, text: string) {
-    this.queryChanged.next(text)
-    data.text = text
-    this.saveData()
-  }
-
-  onTabChange(event: NgbTabChangeEvent) {
-    this.activeTabId = event.nextId
-    this.queryChanged.next(_.find(this.data, ed => ed.title === this.activeTabId).text)
-  }
-
-  editTitle(data: EditorData, modalContent) {
-    this.editingTitle = data.title
-    this.modalService.open(modalContent).result.then(result => {
-      if (_.find(this.data, (d: EditorData) => d.title === result && d !== data)) {
-        alert('You can\'t have duplicate tab names!')
-      } else {
-        data.title = result
-        this.saveData()
-        this.activeTabId = data.title
-      }
-    }).catch(reason => { })
-  }
-
-  closeTab(data: EditorData, event) {
-    event.preventDefault()
-    this.data = _.filter(this.data, d => d !== data)
-    if (this.data.length === 0) {
-      this.data.push({ title: 'untitled-1', text: '' })
-    }
-    this.activeTabId = this.data[0].title
-    this.queryChanged.next(this.data[0].text)
-    this.saveData()
-  }
-
+	textChange(data: EditorData, text: string) {
+		this.queryChanged.next(text)
+		data.text = text
+		this.saveData()
+	}
 }
